a5aeb3e7628cf05231ad99b21da339dc06e8a0fb
Alexandre ZITOUNI TINLOT <58300805+AlexandreZT@users.noreply.github.com>
2020-12-17 00:49:23 +0100

    Add files via upload

---
+++ snake/game/main.py
@@ -0,0 +1,11 @@
+import snake
+
+class Main():
+    """
+    This is the main class for the snake game !
+    """
+    def snake_born():
+        print(snake())
+
+# if __name__ == "__main__":
+Main.snake_born()    
---
+++ snake/game/snake.py
@@ -0,0 +1,11 @@
+import main
+
+class Snake(Main):
+    """
+    About snake
+    """
+    def __init__(self, speed=1, size=1, pos_x=5, pos_y=5):
+        print("C'est partie !")
+    
+    def move():
+        pass
